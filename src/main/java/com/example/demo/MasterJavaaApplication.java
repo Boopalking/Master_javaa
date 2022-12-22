62f8df489e8d9c2b87bce89f06bdb8bb423b1c0d
unknown <boopalswetha98@gmail.com>
2022-12-22 23:30:42 +0530

    first commit

---
+++ src/main/java/com/example/demo/MasterJavaaApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterJavaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterJavaaApplication.class, args);
+		
+		System.out.println("boopal welcome");
+		System.out.println("boopal welcome");
+	}
+
+}
